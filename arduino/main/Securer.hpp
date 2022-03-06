41cb4d19a7ccdee3ca671a4463e25accda43907c
danielmahyar <thediamonds764@gmail.com>
2022-03-06 13:14:49 +0100

    feat: :construction: added basic networking to arduino

---
+++ arduino/main/Securer.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <Keypad.h>
+
+using namespace std;
+
+const byte rows = 4;
+const byte cols = 4;
+char keys[rows][cols] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+class Securer {
+    
+};
